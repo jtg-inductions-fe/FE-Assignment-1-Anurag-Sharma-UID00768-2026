1f64c3453db21fff34fd5382f25478ac92f46817
Anurag sharma <anurag.sharma@joshtechnologygroup.com>
2026-08-08 00:39:25 +0530

    [AS_A1_01]: Fixed Hamburger color Issue

--- src/script/index.js
+++ src/script/index.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('.navbar__menu');
 const mobileMenu = document.querySelector('.mobile__menu');
+const hamIcon = document.querySelector('.icon-hamburger');
 
 hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('move-right');
     hamburger.classList.toggle('color-blue');
+    hamIcon.classList.toggle('color-white');
 });
 
 document.addEventListener('click', (event) => {
@@ -13,6 +15,7 @@ document.addEventListener('click', (event) => {
     ) {
         mobileMenu.classList.remove('move-right');
         hamburger.classList.remove('color-blue');
+        hamIcon.classList.remove('color-white');
     }
 });
 
@@ -25,5 +28,6 @@ mobileLinks.forEach((link) => {
     link.addEventListener('click', () => {
         mobileMenu.classList.remove('move-right');
         hamburger.classList.toggle('color-blue');
+        hamIcon.classList.toggle('color-white');
     });
 });
